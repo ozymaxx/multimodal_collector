357a6188924c92ca5725c3117b9eaed41e7b939f
Ozan Can Altiok <ozymaxx@gmail.com>
2016-07-13 13:23:08 +0300

    coloring problem seems solved

--- app/src/main/java/ku/iui/imotion/socceruserstudy/CanvasView.java
+++ app/src/main/java/ku/iui/imotion/socceruserstudy/CanvasView.java
@@ -17,6 +17,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 import static android.graphics.Color.BLACK;
 import static android.graphics.Color.BLUE;
 import static android.graphics.Color.RED;
@@ -29,11 +31,11 @@ public class CanvasView extends ImageView {
     public int height;
     private Bitmap mBitmap;
     private Canvas mCanvas;
-    private Path mPath;
+    private ArrayList<Path> mPaths;
     Context context;
-    private Paint mPaint;
+    private ArrayList<Paint> mPaints;
     private float mX, mY;
-    private static final float TOLERANCE = 5;
+    private static final float TOLERANCE = 0;
 
     private Sketch sketch;
 
@@ -41,18 +43,28 @@ public class CanvasView extends ImageView {
         super(c, attrs);
         context = c;
 
+        mPaths = new ArrayList<Path>();
+        mPaints = new ArrayList<Paint>();
+
         // we set a new Path
-        mPath = new Path();
+        Path mPath = new Path();
+        mPaths.add(mPath);
 
         // and we set a new Paint with the desired attributes
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setColor(WHITE);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeWidth(4f);
+        mPaints.add(newPaint(WHITE,4f));
 
         sketch = new Sketch();
+    }
+
+    public Paint newPaint(int c,float strokeWidth) {
+        Paint mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(c);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeWidth(strokeWidth);
+
+        return mPaint;
     }
 
     // override onSizeChanged
@@ -70,12 +82,15 @@ public class CanvasView extends ImageView {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         // draw the mPath with the mPaint on the canvas when onDraw
-        canvas.drawPath(mPath, mPaint);
+
+        for (int i = 0; i < mPaths.size(); i++) {
+            canvas.drawPath(mPaths.get(i), mPaints.get(i));
+        }
     }
 
     // when ACTION_DOWN start touch according to the x,y values
     private void startTouch(float x, float y) {
-        mPath.moveTo(x, y);
+        mPaths.get(mPaths.size()-1).moveTo(x, y);
         mX = x;
         mY = y;
 
@@ -88,7 +103,7 @@ public class CanvasView extends ImageView {
         float dx = Math.abs(x - mX);
         float dy = Math.abs(y - mY);
         if (dx >= TOLERANCE || dy >= TOLERANCE) {
-            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mPaths.get(mPaths.size()-1).quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
             mX = x;
             mY = y;
 
@@ -97,7 +112,16 @@ public class CanvasView extends ImageView {
     }
 
     public void clearCanvas() {
-        mPath.reset();
+        for (Path mPath : mPaths) {
+            mPath.reset();
+        }
+
+        mPaths = new ArrayList<Path>();
+        mPaths.add(new Path());
+
+        mPaints = new ArrayList<Paint>();
+        mPaints.add(newPaint(WHITE,4f));
+
         invalidate();
 
         sketch = new Sketch();
@@ -105,10 +129,15 @@ public class CanvasView extends ImageView {
 
     // when ACTION_UP stop touch
     private void upTouch() {
-        mPath.lineTo(mX, mY);
+        mPaths.get(mPaths.size()-1).lineTo(mX, mY);
 
         sketch.addPoint(mX,mY);
         sketch.leaveStroke();
+
+        mPaths.add(new Path());
+
+        Paint mPaint = mPaints.get(mPaints.size()-1);
+        mPaints.add(newPaint(mPaint.getColor(),mPaint.getStrokeWidth()));
     }
 
     //override the onTouchEvent
@@ -135,6 +164,8 @@ public class CanvasView extends ImageView {
     }
 
     public void changeModeAndColor(int color) {
+        Paint mPaint = mPaints.get(mPaints.size()-1);
+
         switch (color) {
             case 1:
                 mPaint.setColor(Color.argb(150,0xcc,0,0));
